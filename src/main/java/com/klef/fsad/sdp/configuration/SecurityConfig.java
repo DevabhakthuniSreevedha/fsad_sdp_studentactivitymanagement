3cf23bdbbd0333dbf45e8c67a6d6c9acfce772df
DevabhakthuniSreevedha <2400030242@kluniversity.in>
2026-04-28 15:18:50 +0530

    v3.0

--- src/main/java/com/klef/fsad/sdp/configuration/SecurityConfig.java
+++ src/main/java/com/klef/fsad/sdp/configuration/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig
     {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://spectacular-strudel-6b4c58.netlify.app"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
